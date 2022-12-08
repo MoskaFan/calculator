9a0c8c4c6dac6d7cd11744140ccd0669e7846be2
Iuliia Atutova <67522690+MoskaFan@users.noreply.github.com>
2022-12-08 12:29:10 +0100

    Add calculate functions with BigDecimal numbers

--- Calculate.java
+++ Calculate.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class calculate {
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+public class Calculate {
+    public static void main(String[] args) {
+        BigDecimal bd1 = BigDecimal.valueOf(0.3);
+        BigDecimal bd2 = BigDecimal.valueOf(0.1);
+
+        BigDecimal result1 = add_2_double_numbers(bd1,bd2);
+        BigDecimal result2 = abstract_2_double_numbers(bd1,bd2);
+        BigDecimal result3 = multiply_2_double_numbers(bd1,bd2);
+        BigDecimal result4 = divide_2_double_numbers(bd1,bd2);
+        System.out.println("Summieren: " + result1 + ", Abstrahieren: " +result2+ ", Multiplizieren: " +result3+ ", Dividieren: " + result4);
+    }
+
+    public static BigDecimal add_2_double_numbers(BigDecimal bd1, BigDecimal bd2) {
+        return bd1.add(bd2,MathContext.DECIMAL32);
+    }
+    public static BigDecimal abstract_2_double_numbers(BigDecimal bd1, BigDecimal bd2) {
+        return bd1.subtract(bd2,MathContext.DECIMAL32);
+    }
+    public static BigDecimal multiply_2_double_numbers(BigDecimal bd1, BigDecimal bd2) {
+        return bd1.multiply(bd2,MathContext.DECIMAL32);
+    }
+    public static BigDecimal divide_2_double_numbers(BigDecimal bd1, BigDecimal bd2) {
+        return bd1.divide(bd2,MathContext.DECIMAL32);
+    }
+
+
 }
--- calculateTest.java
+++ calculateTest.java
@@ -1,4 +1,43 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class calculateTest {
-  
+@Test
+    void when_add_2_double_numbers_then_result(){
+    BigDecimal bd1 = new BigDecimal(0.3);
+    BigDecimal bd2 = new BigDecimal(0.1);
+    BigDecimal expected = bd1.add(bd2, MathContext.DECIMAL32);
+    BigDecimal result = Calculate.add_2_double_numbers(bd1, bd2);
+    assertEquals(expected, result);
+}
+@Test
+    void when_abstract_2_double_numbers_then_result(){
+        BigDecimal bd1 = new BigDecimal(0.3);
+        BigDecimal bd2 = new BigDecimal(0.1);
+        BigDecimal expected = bd1.subtract(bd2, MathContext.DECIMAL32);
+        BigDecimal result = Calculate.abstract_2_double_numbers(bd1, bd2);
+        assertEquals(expected, result);
+    }
+    @Test
+
+    void when_multiply_2_double_numbers_then_result(){
+        BigDecimal bd1 = new BigDecimal(0.3);
+        BigDecimal bd2 = new BigDecimal(0.1);
+        BigDecimal expected = bd1.multiply(bd2, MathContext.DECIMAL32);
+        BigDecimal result = Calculate.multiply_2_double_numbers(bd1, bd2);
+        assertEquals(expected, result);
+    }
+    @Test
+    void when_divide_2_double_numbers_then_result(){
+        BigDecimal bd1 = new BigDecimal(0.3);
+        BigDecimal bd2 = new BigDecimal(0.1);
+        double bd3 = 3.0;
+        BigDecimal expected = bd1.divide(bd2, MathContext.DECIMAL32);
+        BigDecimal result = Calculate.divide_2_double_numbers(bd1, bd2);
+        assertEquals(expected, result);
+    }
 }
